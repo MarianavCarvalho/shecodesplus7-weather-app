174a3496d59b4bd6b8a6fd42f4c8f3e4782b7ffd
MarianavCarvalho <marianavcarvalho97@gmail.com>
2023-05-09 18:53:29 +0100

    Added icon from API js

--- src/app.js
+++ src/app.js
@@ -37,6 +37,10 @@ function showTemperature(response){
   
   let currentDate = document.querySelector("#date");
 currentDate.innerHTML = formatDate(response.data.time * 1000);
+
+let iconElement = document.querySelector("#icon");
+iconElement.setAttribute("src" , `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`)
+iconElement.setAttribute("alt" , response.data.condition.description);
 }
 
 
